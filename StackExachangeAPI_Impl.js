24ec9e29660a76fd8f140fae884caed4acbf347d
Sachin <sachin@jain024gmail.com>
2013-10-27 10:48:00 +0530

    SW-14 [partial] Creates StackExchangeAPI_Impl.js for implementing stack exchange api methods

---
+++ StackExachangeAPI_Impl.js
@@ -0,0 +1,8 @@
+window.SW = window.SW || {};
+SW.methods = SW.methods || {};
+SW.vars = SW.vars || {};
+SW.stores = SW.stores || {};
+SW.callbacks = SW.callbacks || {};
+
+/*-----------------------------------------------------------*/
+
