cf32496e7265e65ea8b21b2f1509cd16bacb7f65
Sachin <sachin@jain024gmail.com>
2013-11-05 13:34:02 +0530

    @trivial Removes extra unused file

--- StackExachangeAPI_Impl.js
+++
@@ -1,8 +0,0 @@
-window.SW = window.SW || {};
-SW.methods = SW.methods || {};
-SW.vars = SW.vars || {};
-SW.stores = SW.stores || {};
-SW.callbacks = SW.callbacks || {};
-
-/*-----------------------------------------------------------*/
-
