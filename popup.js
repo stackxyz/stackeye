aad200bf5ca1726f0c27e81a1ef20f8f34dbdec3
Sachin <sachin@jain024gmail.com>
2013-11-17 14:25:35 +0530

    #13 Render notifications on popup

--- popup.js
+++ popup.js
@@ -4,9 +4,11 @@ Popup.methods = {};
 Popup.vars = {};
 
 Popup.vars.busyWatching = false;
+Popup.vars.numNotificationsToShow = 5;
 Popup.vars.$watchButton = $('#swo_watch_button');
 Popup.vars.$watchingButton = $('#swo_watching_button');
 Popup.vars.$notificationCountButton = $('#notification-count');
+Popup.vars.$notificationList = $('#notification-area').find('.notification-list');
 Popup.vars.notifications = BG.SW.stores.notificationStore;
 
 Popup.methods.watchSuccess = function(message) {
@@ -35,8 +37,45 @@ Popup.methods.setNotificationCount = function() {
   Popup.vars.$notificationCountButton.text(num_notifications);
 };
 
+Popup.methods.getNotificationToShow = function(notificationObject) {
+  var text = '',
+    markup = '',
+    numAnswers = notificationObject.numAnswers,
+    numComments = notificationObject.numComments;
+
+  if (numAnswers != 0 && numComments != 0) {
+    text = '<span class="bold">' + numAnswers + ' answers and ' + numComments + ' comments <span>';
+  } else if (numAnswers !=0 && numComments == 0) {
+    text = '<span class="bold">' + numAnswers + ' answers <span>';
+  } else if (numAnswers == 0 && numComments != 0) {
+    text = '<span class="bold">' + numComments + ' comments <span>';
+  }
+
+  markup = '<div class="upper-row">' + text + ' on' + '</div>';
+  markup += '<div class="lower-row">' + 
+            '<a href="' + notificationObject.link + '">' + notificationObject.title + '</a>' +
+            '</div>';
+
+  return markup;
+}
+
+Popup.methods.renderNotifications = function() {
+  // TODO @Sachin: Use document fragment here
+  var notificationList = Popup.vars.notifications,
+    notificationListLength = Popup.vars.notifications.length,
+    notificationToShow;
+
+  // TODO @Sachin: Sort the notification list by latest answer/comment
+
+  for (var i = 0; i < notificationListLength && i < Popup.vars.numNotificationsToShow; i++) {
+    notificationToShow = Popup.methods.getNotificationToShow(notificationList[i]);
+    $('<li>').html(notificationToShow).appendTo(Popup.vars.$notificationList);
+  }
+};
+
 Popup.methods.updateCurrentPage = function() {
   Popup.methods.setNotificationCount();
+  Popup.methods.renderNotifications();
 };
 
 Popup.methods.init = function() {
