d1f53115670a5716574df2156ae5d38b623aba73
Sachin Jain <sachinjain024@gmail.com>
2014-02-01 16:40:24 +0530

    v1.2 Released

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
     zipup: {
       package: {
         appName: 'StackEye',
-        version: '1.1.2',
+        version: '1.2',
         files: [
           { cwd: 'src', src: '**', expand: true, dest: 'src' },
           { cwd: 'resources', src: '**', expand: true, dest: 'resources' },
