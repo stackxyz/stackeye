21e99e9758c2349569f57431d4386cfbea126a37
Sachin Jain <sachinjain024@gmail.com>
2019-02-13 15:18:47 +0530

    Released v19.2.13

--- src/shared/globals.js
+++ src/shared/globals.js
@@ -61,7 +61,7 @@ SW.constants = {
   ANSWER          : 'answer',
   URL_ROOT        : 'http://api.stackexchange.com/2.2/',
   APP_KEY         : 'MKhHgtFFPM)vQAp2pvu)fw((',
-  VERSION         : '19.2.12'
+  VERSION         : '19.2.13'
 };
 
 SW.filters = {
